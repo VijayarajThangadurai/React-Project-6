e9f8ac0f6e153eb93cc57d9c57407ac51a6da91b
VijayarajThangadurai <124797617+VijayarajThangadurai@users.noreply.github.com>
2023-06-25 20:04:55 +0530

    Making a hard Coded Cart without any Functionality

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -1,11 +1,11 @@
-import MealItemForm from './MealItemForm';
-import classes from './MealItem.module.css';
 
+import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 const MealItem =(props)=>{
-    const price =`$${props.price.toFixed(2)}`
+    const price =`$${props.price.toFixed(2)}`;
 return ( 
-<li>
-<div className={classes.meals}>
+<li className={classes.meal}>
+<div >
     <h3>{props.name}</h3>
     <div className={classes.description}>{props.description}</div>
     <div className={classes.price}>{price}</div>
@@ -14,7 +14,7 @@ return (
     <MealItemForm/>
 </div>
 </li>
-)
+);
 };
 
 export default MealItem;
